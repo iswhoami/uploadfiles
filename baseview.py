76aebe9d9e9410d05abac29e0c54eb9c2817fc80
iswhoami <iswhoamirus@gmail.com>
2024-11-08 14:55:15 +0300

    Добавлен базовы класс View

---
+++ baseview.py
@@ -0,0 +1,27 @@
+from flask import request
+from flask.views import MethodView
+
+from connection import Connection
+
+
+class BaseView(MethodView):
+
+    EXTENSION = ['jpg', 'png']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._conn = Connection()
+
+    def parse_file(self, argname, default=None):
+        file = request.files.get(argname, default)
+        return file
+
+    def parse_int(self, argname, default=None):
+        value = request.values.get(argname, default)
+        try:
+            return int(value)
+        except ValueError:
+            return default
+
+    def parse_str(self, argname, default=None):
+        return request.values.get(argname, default)
